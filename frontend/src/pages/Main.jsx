1086256e3ed039cf5f29ea7e4882d1b1387952ae
soyeeee2 <soyeeee2@gmail.com>
2023-06-02 00:17:54 +0900

    navbar + slideimg01

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,8 +1,11 @@
+import { Navbar } from "components/navbar/index";
+
 import slide01 from 'img/slide01.png'
 
 const Main = () => {
   return(
     <>
+      <Navbar></Navbar>
       <img src={slide01}></img>
     </>
   ) 
