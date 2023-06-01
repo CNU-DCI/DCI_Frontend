1eb71a252db570a8a2eed04da0879ed4741dd1ab
narlo23 <aks152312@gmail.com>
2023-06-02 00:21:55 +0900

    이미지 사이즈 수정

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import { FaSistrix } from "react-icons/fa";
-import header from 'img/header.png'
+import header from "img/header.png";
 
 const HeaderSize = styled.div`
   width: 100%;
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <>
       <HeaderSize>
-        <img src={header}></img>
+        <img src={header} style={{ width: "100%" }}></img>
       </HeaderSize>
     </>
   );
--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,14 +1,14 @@
 import { Navbar } from "components/navbar/index";
 
-import slide01 from 'img/slide01.png'
+import slide01 from "img/slide01.png";
 
 const Main = () => {
-  return(
+  return (
     <>
       <Navbar></Navbar>
-      <img src={slide01}></img>
+      <img src={slide01} style={{ width: "100%" }}></img>
     </>
-  ) 
+  );
 };
 
 export default Main;
